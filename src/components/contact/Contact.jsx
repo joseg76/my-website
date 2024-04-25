0d0edf2e570c74b8b61fcd604066e2513886b795
Jose gomez <josegomez@Joses-MacBook-Pro.local>
2024-04-25 13:53:29 -0600

    send

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -58,7 +58,7 @@ const Contact = () => {
         </div>
 
         <div className="contact__content">
-          <h3 className="contact__title">Write me your project</h3>
+          <h3 className="contact__title">Send me your project idea</h3>
 
           <form ref={form} onSubmit={sendEmail} className="contact__form">
             <div className="contact__form-div">
